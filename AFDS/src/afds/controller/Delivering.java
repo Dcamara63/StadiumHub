9e00330b73d3b41a56814a415cfc01d96ef571af
Kaan <Kaan@10.84.150.161>
2014-02-11 19:18:47 -0800

    badass motherfucker

--- AFDS/src/afds/controller/Delivering.java
+++ AFDS/src/afds/controller/Delivering.java
@@ -1,6 +1,8 @@
 package afds.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,19 +14,43 @@ import javax.servlet.http.HttpServletResponse;
 public class Delivering extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public Delivering() {
-        super();
-    }
+	public Delivering() {
+		super();
+	}
 
 	public void init(ServletConfig config) throws ServletException {
 
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Delivering</title></head><body>");
+
+		out.println("<h2>Delivery Module</h2>");
+		out.println("<table border='1'>");
+		out.println("<tr><th>Order_ID</th><th>Operations</th></tr>");
+		
+		//get order id, operations include "order_details", "cancel_order" and "order_completed"?
+		
+		/*out.println("<tr><td>" + entry.getName() + " says: </td><td>"
+				+ entry.getMessage() + "</td>" + "<td><a href='EditEntry?id=" + entry.getId()
+				+ "'>Edit</a> | <a href='DeleteEntry?id=" + entry.getId() + "'>Delete</a></td></tr>");*/
+
+		out.println("</table>");
+
+		//out.println("<p><a href='AddComment'>Leave a comment</a></p>");
+		
+
+		out.println("</body></html>");
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
 	}
 
