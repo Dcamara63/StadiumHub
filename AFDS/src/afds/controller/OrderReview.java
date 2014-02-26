6507af59890c9a27e63bcbc2580e18c8051dde2d
harry520 <harryjiang_520@hotmail.com>
2014-02-25 19:28:42 -0800

    Some files updated.

--- AFDS/src/afds/controller/OrderReview.java
+++ AFDS/src/afds/controller/OrderReview.java
@@ -47,16 +47,16 @@ public class OrderReview extends HttpServlet {
 			// products.getName();
 			for (LocationEntry location : locations) {
 				if (location.getSeatId() % 2 == 1) {
-					Kitchen1Entry order1 = new Kitchen1Entry(id++, products);
-					List<Kitchen1Entry> orders1 = (List<Kitchen1Entry>) getServletContext()
-							.getAttribute("order1s");
-					orders1.add(order1);
+					Kitchen1Entry order = new Kitchen1Entry(id++, products);
+					List<Kitchen1Entry> orders = (List<Kitchen1Entry>) getServletContext()
+							.getAttribute("orders");
+					orders.add(order);
 				}
 				if (location.getSeatId() % 2 == 0) {
-					Kitchen2Entry order2 = new Kitchen2Entry(id++, products);
-					List<Kitchen2Entry> orders2 = (List<Kitchen2Entry>) getServletContext()
-							.getAttribute("orders2");
-					orders2.add(order2);
+					Kitchen2Entry order = new Kitchen2Entry(id++, products);
+					List<Kitchen2Entry> orders = (List<Kitchen2Entry>) getServletContext()
+							.getAttribute("orders");
+					orders.add(order);
 				}
 			}
 			response.sendRedirect("registration/UserLogin");
